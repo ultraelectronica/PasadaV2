63b4263ac1acb63fcf00c7a391bc1e4ac4268e2d
ultraelectronica <fyketonel@gmail.com>
2026-02-23 01:01:46 +0800

    Refactor the main application entry point to `PasadaApp` and integrate Compose Navigation.

---
+++ app/src/main/java/com/example/pasada/ui/PasadaApp.kt
@@ -0,0 +1,28 @@
+package com.example.pasada.ui
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.pasada.Greeting
+
+@Composable
+fun PasadaApp() {
+    val navController = rememberNavController()
+    
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = "home",
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable("home") {
+                Greeting(name = "Pasada User")
+            }
+        }
+    }
+}
